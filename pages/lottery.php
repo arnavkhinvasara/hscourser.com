852d1e17aeea99cea9828b78536f6b61e1943448
root <root@ip-172-31-83-140.ec2.internal>
2021-08-07 05:57:45 +0000

    fixing lottery page

--- pages/lottery.php
+++ pages/lottery.php
@@ -1,20 +1,20 @@
 <?php
 	$ticketsArray = [];
+	$usersArray = [];
 	require_once("connection.php");
 	$statement = "SELECT * FROM userdata";
 	$q_statement = mysqli_query($conn, $statement);
 	while ($row = mysqli_fetch_assoc($q_statement)){
 		array_push($ticketsArray, $row["Fullname"], $row["Referred"]);
+		array_push($usersArray, $row["Fullname"]);
 	}
 	foreach($ticketsArray as $key => $value){
-		if ($value=="" || str_contains($value, "arnav")==false || str_contains($value, "Arnav")==false){
+		if ($value==""){
 			unset($ticketsArray[$key]);
 		}
 	}
 	$peopleArray = array_values($ticketsArray);
-	print_r($peopleArray);
-	echo "<br/>";
-	echo (count($ticketsArray));
-	echo "<br/>";
 	echo ($peopleArray[rand(0, count($ticketsArray))]);
+	echo "<br/><br/><br/>";
+	echo count($usersArray);
 ?>
